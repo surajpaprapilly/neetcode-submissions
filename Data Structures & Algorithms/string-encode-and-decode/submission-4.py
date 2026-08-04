9718fd27e8161472ec947e272bdd63a4fe4ec94f
Suraj <77538833+surajpaprapilly@users.noreply.github.com>
2026-08-04 23:09:54 +0800

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,33 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        encoded_string = ""
+        #strs=["The quick brown fox","jumps over the","lazy dog","1234567890","abcdefghijklmnopqrstuvwxyz"]
+        for s in strs:
+            encoded_string += str(len(s)) + "#" + s
+        #print(encoded_string)
+        return encoded_string
+
+
+    def decode(self, s: str) -> List[str]:
+        decoded = []
+        #s = "19#The quick brown fox14#jumps over the8#lazy dog10#123456789026#abcdefghijklmnopqrstuvwxyz"
+        i = 0
+        while i < len(s)-1:
+            j = i
+            while s[j] != "#": # e.g 5#Hello5#World, 
+                j += 1         #     0123456789
+            num_str = ""
+            #print(i)
+            #print(j)
+            for k in range(i,j):
+                num_str += s[k]
+            num_int = int(num_str)
+            decoded.append(s[j+1:j+num_int+1])
+            i = j + num_int + 1
+        return decoded
+
+# strs=["we","say",":","yes","!@#$%^&*()"]
+# solution = Solution()
+# encoded_string = solution.encode(strs);
+# decoded_strs = solution.decode(encoded_string);
