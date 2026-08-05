cb84d339c2a1e2b374c7f8df02463546b6ab53c1
Suraj <77538833+surajpaprapilly@users.noreply.github.com>
2026-08-05 22:14:26 +0800

    Add: products-of-array-discluding-self - submission-5

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-5.py
@@ -0,0 +1,34 @@
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        # Naive approach: For each index multiply all remaining values O(N^2)
+        # Another approach: Multiply all numbers and divide by nums[i]
+        # If there is 0 then product of all numbers will be 0 (except the position(s) of 0)
+
+        # If there is 0 then you cannot divide by nums[i] when nums[i] = 0. Handle as special case
+
+        # Pre fix Post fix approach:
+        # First pass calculate the prefix sum
+        # Second pass multiply by the postfix sum at each index
+
+
+        res = [1]
+        n = len(nums)
+        for i in range(1,n):
+            res.append(nums[i - 1]*res[i - 1])
+        
+        postfix_sum = 1
+        for j in range(n - 1,-1,-1):
+            res[j] *= postfix_sum
+            postfix_sum *= nums[j]
+        return res
+
+# Time complexity: O(N) --> 1 pass for Prefix and 1 pass for Postfix
+
+# Space complexity: O(1) --> Only output array as the additional memory used, which is not counted.
+
+
+
+
+
+
+
